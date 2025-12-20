09cca7d61f209dc92273f344bdc8e4734f91bd02
José Isac <joseisac@tutamail.com>
2025-12-20 14:31:29 -0300

    include/lexer.h: create lexer header
    Declare the lexer and token structures, define the token types and
    declare some functions.

---
+++ include/lexer.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2025 José Isac Araujo Monção
+ * All rights reserved.
+ *
+ * See LICENSE file for copyright and license details.
+ */
+#ifndef LEXER_H
+#define LEXER_H
+
+typedef enum {
+  TOKEN_EOF,
+  TOKEN_ALGORITHM,
+  TOKEN_ENDPROGRAM,
+  TOKEN_BEGIN,
+  TOKEN_VARIABLES,
+  TOKEN_INT_TYPE,
+  TOKEN_REAL_TYPE,
+  TOKEN_BOOL_TYPE,
+  TOKEN_STR_TYPE,
+  TOKEN_CHAR_TYPE,
+  TOKEN_IF,
+  TOKEN_THEN,
+  TOKEN_ELSE,
+  TOKEN_ENDIF,
+  TOKEN_SWITCH,
+  TOKEN_CASE,
+  TOKEN_ENDSWITCH,
+  TOKEN_WHILE,
+  TOKEN_DO,
+  TOKEN_ENDWHILE,
+  TOKEN_FOR,
+  TOKEN_ENDFOR,
+  TOKEN_ASSIGN,
+  TOKEN_PLUS,
+  TOKEN_MINUS,
+  TOKEN_MULTIPLY,
+  TOKEN_DIVIDE,
+  TOKEN_MOD,
+  TOKEN_RAD,
+  TOKEN_EQUAL,
+  TOKEN_DIFF,
+  TOKEN_GREATER,
+  TOKEN_LESSER,
+  TOKEN_GEQUAL,
+  TOKEN_LEQUAL,
+  TOKEN_AND,
+  TOKEN_OR,
+  TOKEN_NOT,
+  TOKEN_READ,
+  TOKEN_WRITE,
+  TOKEN_ALGNAME,
+  TOKEN_IDENTIFIER,
+  TOKEN_LPAREN,
+  TOKEN_RPAREN,
+  TOKEN_COMMA,
+  TOKEN_COLON,
+  TOKEN_SEMICOLON,
+  TOKEN_QUOTE,
+  TOKEN_DBQUOTES,
+  TOKEN_COMMENT,
+  TOKEN_ERROR
+} token_t;
+
+typedef struct {
+  token_t type;
+  char lexeme[100];
+  int line;
+  int col;
+} token;
+
+typedef struct {
+  const char* source;
+  int pos;
+  int line;
+  int col;
+} lexer;
+
+void lexer_init(lexer *lex, const char *source);
+token lexer_next_token(lexer *lex);
+void token_print(const token *tkn);
+
+#endif
