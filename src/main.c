0596b2b44301a59a6fd1b58d25deb30305701ab8
José Isac <joseisac@tutamail.com>
2025-12-20 13:46:19 -0300

    src/main.c: create main.c file
    Hello, world!

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 José Isac Araujo Monção
+ * All rights reserved.
+ *
+ * See LICENSE file for copyright and license details.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  puts("Hello, world!");
+  return EXIT_SUCCESS;
+}
