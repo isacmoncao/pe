772c99856097731e94d2f4fbc2c07dbca7264f2b
José Isac <joseisac@tutamail.com>
2025-12-20 14:39:53 -0300

    src/lexer.c: begins the lexer implementation
    Create three functions: one to init the lexer, other to get the current
    char and advance, and another to just take a look at the current char.

---
+++ src/lexer.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 José Isac Araujo Monção
+ * All rights reserved.
+ *
+ * See LICENSE file for copyright and license details.
+ */
+#include "../include/lexer.h"
+
+void lexer_init(lexer *lex, const char *source) {
+  lex->source = source;
+  lex->pos = 0;
+  lex->line = 1;
+  lex->col = 1;
+}
+
+static char lexer_advance(lexer *lex) {
+  char c = lex->source[lex->pos++];
+  if (c == '\n') {
+    lex->line++;
+    lex->col = 1;
+  } else {
+    lex->col++;
+  }
+  return c;
+}
+
+// Look the current char
+static char lexer_peek(lexer *lex) {
+  return lex->source[lex->pos];
+}
