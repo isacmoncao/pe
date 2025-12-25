123bcc999bba0b839988b0e9c6293e9372ea8d6e
José Isac <joseisac@tutamail.com>
2025-12-25 16:26:55 -0300

    src/util.c: implements usage and err_file functions

--- src/util.c
+++ src/util.c
@@ -1,5 +1,6 @@
 #include "../include/util.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int count_bytes(FILE *file) {
   fseek(file, 0, SEEK_SET);
@@ -19,4 +20,16 @@ void file_to_char_arr(char *arr, FILE *file) {
     i++;
   }
   arr[i] = '\0';
+}
+
+void usage(void) {
+    fprintf(stderr, "usage: %s <source_file>\n", getprogname());
+    exit(EXIT_FAILURE);
+}
+
+void err_file(const char *file) {
+    fprintf(stderr, "%s: error while opening %s\n"
+            "Does the file exists or do you have reading permissions?\n",
+            getprogname(), file);
+    exit(EXIT_FAILURE);
 }
