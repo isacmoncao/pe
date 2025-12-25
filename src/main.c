8a2053546f00f1605aa37dd9773135396e939a8a
José Isac <joseisac@tutamail.com>
2025-12-25 16:31:03 -0300

    src/main.c: read source code and lex it

--- src/main.c
+++ src/main.c
@@ -5,8 +5,35 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <err.h>
+#include "../include/lexer.h"
+#include "../include/util.h"
 
-int main(void) {
-  puts("Hello, world!");
+static lexer lex;
+static char *code;
+static FILE *source_file;
+static token tkn;
+
+int main(int argc, char *argv[]) {
+  setprogname(argv[0]);
+
+  if (argc != 2)
+    usage();
+  
+  source_file = fopen(argv[1], "r");
+  if ( source_file == NULL)
+    err_file(argv[1]);
+
+  code = malloc(count_bytes(source_file) * sizeof(char));
+  file_to_char_arr(code, source_file);
+  lexer_init(&lex, code);
+
+  do {
+    tkn = lexer_next_token(&lex);
+    token_print(tkn);
+  } while (tkn.type != TOKEN_EOF);
+
+  free(code);
+  fclose(source_file);
   return EXIT_SUCCESS;
 }
